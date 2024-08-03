f79c132f4eefab8dcc47ba2e849266b3b373e59f
IRSAGE <sibo.egide98@gmail.com>
2024-08-03 16:31:24 +0200

    Managing App-wide State & Data

--- lib/screens/tabs.dart
+++ lib/screens/tabs.dart
@@ -16,12 +16,28 @@ class _TabScreenState extends State<TabsScreen> {
   int _selectedPageIndex = 0;
 
   final List<Meal> _favoriteMeals = [];
+
+  void _showInfoMessage(String message) {
+    ScaffoldMessenger.of(context).clearSnackBars();
+    ScaffoldMessenger.of(context).showSnackBar(
+      SnackBar(
+        content: Text(message),
+      ),
+    );
+  }
+
   void _toggleMealFavoriteStatus(Meal meal) {
     final isExisting = _favoriteMeals.contains(meal);
     if (isExisting) {
-      _favoriteMeals.remove(meal);
+      setState(() {
+        _favoriteMeals.remove(meal);
+        _showInfoMessage('Meal removed from favorites.');
+      });
     } else {
-      _favoriteMeals.add(meal);
+      setState(() {
+        _favoriteMeals.add(meal);
+        _showInfoMessage('Meal added to favorites.');
+      });
     }
   }
 
@@ -38,7 +54,7 @@ class _TabScreenState extends State<TabsScreen> {
 
     if (_selectedPageIndex == 1) {
       activePage = MealsScreen(
-        meals: [],
+        meals: _favoriteMeals,
         onToggleFavorites: _toggleMealFavoriteStatus,
       );
     }
