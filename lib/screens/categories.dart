177cbf10b25eb4b52eb21bd4ae1e152562af9ce2
IRSAGE <sibo.egide98@gmail.com>
2024-08-03 19:16:08 +0200

    Applying Filters

--- lib/screens/categories.dart
+++ lib/screens/categories.dart
@@ -6,12 +6,17 @@ import 'package:meals_app/screens/meals.dart';
 import 'package:meals_app/widgets/category_grid_item.dart';
 
 class CategoriesScreen extends StatelessWidget {
-  const CategoriesScreen({super.key, required this.onToggleFavorites});
+  const CategoriesScreen({
+    super.key,
+    required this.onToggleFavorites,
+    required this.availableMeals,
+  });
 
   final void Function(Meal meal) onToggleFavorites;
+  final List<Meal> availableMeals;
 
   void _selectCategory(BuildContext context, Category category) {
-    final filteredMeals = dummyMeals
+    final filteredMeals = availableMeals
         .where((meal) => meal.categories.contains(category.id))
         .toList();
     Navigator.push(
