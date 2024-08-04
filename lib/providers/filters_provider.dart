a6ed9757529981462ba7bf27bdd02736c7cde792
IRSAGE <sibo.egide98@gmail.com>
2024-08-04 10:51:20 +0200

    Creating Filters Grobale state

---
+++ lib/providers/filters_provider.dart
@@ -0,0 +1,29 @@
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+enum Filter {
+  glutenFree,
+  lactoseFree,
+  vegetarian,
+  vegan,
+}
+
+class FiltersNofifier extends StateNotifier<Map<Filter, bool>> {
+  FiltersNofifier()
+      : super({
+          Filter.glutenFree: false,
+          Filter.lactoseFree: false,
+          Filter.vegetarian: false,
+          Filter.vegan: false,
+        });
+
+  void setFilter(Filter filter, bool isActive) {
+    state = {
+      ...state,
+      filter: isActive,
+    };
+  }
+}
+
+final filtersProvider =
+    StateNotifierProvider<FiltersNofifier, Map<Filter, bool>>(
+        (ref) => FiltersNofifier());
